9ab35b256954c368dfcad7d1d02d9263aa93be22
GAMoiso <moiso.giacomo.alberto@iisgalvanimi.edu.it>
2020-01-21 11:44:23 +0000

    Punto 1

--- angular-data/src/app/user-item/user-item.component.ts
+++ angular-data/src/app/user-item/user-item.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
-  selector: 'app-user-item',
-  templateUrl: './user-item.component.html',
-  styleUrls: ['./user-item.component.css']
+selector: 'app-user-item',
+templateUrl: './user-item.component.html',
+styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+name: string; // <-- aggiunta la proprietà name
+constructor() {
+this.name = 'Felipe'; // setta il nome nel costruttore
+}
+ngOnInit() {
+}
 }
